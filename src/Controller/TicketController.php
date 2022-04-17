5fb706a9b069788d64e7d5eaab6dcc56ecfd6c54
Benjamin Venezia <benjamin.venezia@crea-inseec.com>
2022-04-17 16:28:14 +0200

    separate ticket template and beginning adding crud on it

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -36,4 +36,29 @@ class TicketController extends AbstractController
             'adminToken' => $adminLinkToken
         ]);
     }
+
+    #[Route('/{linkToken}/ticket/{id}/delete', name: 'tickets_delete')]
+    public function delete($adminLinkToken, GroupRepository $groupRepository, EventRepository $eventRepository, TicketRepository $ticketRepository): Response
+    {
+        //find event 
+        $event = $eventRepository->findOneBy([
+            'adminLinkToken' => $adminLinkToken
+        ]);
+        //find groups by event
+        $groups = $groupRepository->findBy([
+            'event' => $event->getId(), 
+        ], ['last_helped' => 'ASC']);
+        //grasp tickets
+        $tickets = [];
+        foreach ($groups as $g){
+            $ticketsForThisGroup = $ticketRepository->findBy(['group_ticket_id' => $g->getId()]);
+            array_push($tickets, $ticketsForThisGroup);
+        }
+
+        return $this->render('navigation/eventtickets.html.twig', [
+            'tickets' => $tickets,
+            'event' => $event,
+            'adminToken' => $adminLinkToken
+        ]);
+    }
 }
