03694f888fea05c500b813e775c573d406eaa8c3
Benjamin Venezia <benjamin.venezia@crea-inseec.com>
2022-04-17 20:26:25 +0200

    beginning adding switch status

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\EventRepository;
 use App\Repository\GroupRepository;
+use App\Repository\StatusRepository;
 use App\Repository\TicketRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -48,6 +49,42 @@ class TicketController extends AbstractController
     {
         //find ticket 
         $ticket = $ticketRepository->find($ticketId);
+
+        if(!$ticket) {
+            throw new NotFoundHttpException("Le ticket que vous souhaitez supprimer n'existe pas.");
+        }
+
+        $em->remove($ticket);
+        $em->flush();
+
+        return $this->redirectToRoute('group_show', ['linkTokenParam' => $linkToken]);
+
+    }
+
+    #[Route('/{linkToken}/ticket/{ticketId}/changestatus/${idStatus}', name: 'ticket_changestatus')]
+    public function changeStatus(string $linkToken, $ticketId,EntityManagerInterface $em, TicketRepository $ticketRepository, StatusRepository $statusRepository): Response
+    {
+        //find ticket 
+        $ticket = $ticketRepository->find($ticketId);
+
+        if(!$ticket){
+            throw new NotFoundHttpException("Pas de ticket avec l'identifant " . $ticketId);
+        }
+
+        $status = $statusRepository->findOneBy(['ticket_status' => $ticket->getId()]);
+
+        if(!$status){
+            throw new NotFoundHttpException("Pas de status avec l'identifant ");
+        }
+
+
+
+        $status->setIsArchived(!$status->getIsArchived());
+       
+        // $status = ->getTicketStatus();
+        dd($status);
+        //permet de retrouver le ticket lié au status.
+        // dd($status[0]->getTicketStatus());
 
         if(!$ticket) {
             throw new NotFoundHttpException("Le ticket que vous souhaitez supprimer n'existe pas.");
